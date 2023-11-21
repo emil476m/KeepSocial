373894bb476da0bf3341032f9e009c5ab9f4fa23
TheDarkSide1992 <Jenscp171@gmail.com>
2023-11-21 13:06:37 +0100

    security auth

--- API/Controllers/ChatController.cs
+++ API/Controllers/ChatController.cs
@@ -1,9 +1,9 @@
-﻿using Infastructure;
+﻿using API.Filters;
+using Infastructure;
 using Microsoft.AspNetCore.Mvc;
 using Service;
 
 namespace API.Controllers;
-
 
 
 [Route("api/[controller]")]
@@ -11,6 +11,7 @@ namespace API.Controllers;
 public class ChatController : ControllerBase
 {
     private readonly ChatService _chatService;
+    
 
     public ChatController(ChatService chatService)
     {
@@ -18,26 +19,29 @@ public class ChatController : ControllerBase
     }
     
     [HttpGet("/Rooms")]
+    [RequireAuthentication]
     public IEnumerable<Rooms> getAllRooms(int pageNumber)
     {
-        //var session = HttpContext.GetSessionData()!;
-        int userid = 111;
+        var session = HttpContext.GetSessionData()!;
+        int userid = session.UserId;
         int limit = 10;
         return _chatService.getChatRooms(pageNumber, userid);
     }
 
     [HttpGet("/ChatMessages{roomId}")]
+    [RequireAuthentication]
     public IEnumerable<Message> getMessagesInChat([FromRoute] int roomId, int pageNumber)
     {
-        //var session = HttpContext.GetSessionData()!;
-        int userId = 111;
-        return _chatService.getChats(roomId, pageNumber, userId);
+        var session = HttpContext.GetSessionData()!;
+        int userid = session.UserId;
+        return _chatService.getChats(roomId, pageNumber, userid);
     }
     
     [HttpPost("/SenndMessage")]
+    [RequireAuthentication]
     public Message snedMessagesInChat([FromBody]Message message)
     {
-        //var session = HttpContext.GetSessionData()!;
+        var session = HttpContext.GetSessionData()!;
         int userId = 111;
         return _chatService.sendMessage(message, userId);
     }
