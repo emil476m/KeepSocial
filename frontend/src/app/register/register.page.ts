2dc250587e989e0d88e8e9864868e16913164799
andreas14101 <59689350+andreas14101@users.noreply.github.com>
2023-12-12 10:28:11 +0100

    refactor of terms of service (#93)

--- frontend/src/app/register/register.page.ts
+++ frontend/src/app/register/register.page.ts
@@ -21,42 +21,58 @@ import {environment} from "../../environments/environment.prod";
         <ion-grid>
           <ion-row>
             <ion-col>
-              <ion-item >
-              <ion-input [formControl]="AName" data-testid="accountName_" type="text" label-placement="floating" label="name" ></ion-input>
+              <ion-item>
+                <ion-input [formControl]="AName" data-testid="accountName_" type="text" label-placement="floating"
+                           label="name"></ion-input>
                 <div *ngIf="AName.invalid && AName.touched" class="error">
                   Name must be between 1-100 letters
                 </div>
               </ion-item>
-              <ion-item >
-                <ion-input [formControl]="AEmail" data-testid="accountEmail_" type="text" onpaste="return false;" ondrop="return false;" autocomplete="off" label-placement="floating" label="mail" ></ion-input>
+              <ion-item>
+                <ion-input [formControl]="AEmail" data-testid="accountEmail_" type="text" onpaste="return false;"
+                           ondrop="return false;" autocomplete="off" label-placement="floating"
+                           label="mail"></ion-input>
                 <div *ngIf="AEmail.invalid && AEmail.touched" class="error">
                   Must be a valid Email
                 </div>
               </ion-item>
-              <ion-item >
+              <ion-item>
                 <ion-input type="date" [formControl]="ADate"></ion-input>
               </ion-item>
-              <ion-item >
-                <ion-input [formControl]="APassword" data-testid="accountPassword_" type="text" onpaste="return false;" ondrop="return false;" autocomplete="off" label-placement="floating" label="password" [type]="hide ? 'password' : 'text'" required></ion-input>
+              <ion-item>
+                <ion-input [formControl]="APassword" data-testid="accountPassword_" type="text" onpaste="return false;"
+                           ondrop="return false;" autocomplete="off" label-placement="floating" label="password"
+                           [type]="hide ? 'password' : 'text'" required></ion-input>
                 <ion-icon slot="end" [name]="hide ? 'eye' : 'eye-off'" (click)="hide = !hide"></ion-icon>
                 <div *ngIf="APassword.invalid && APassword.touched" class="error">
                   Must be between8-32 carachters long and contain atleast 1 capital letter
                 </div>
               </ion-item>
-              <ion-item >
-                <ion-input  [formControl]="APasswordRepeat" data-testid="accountPasswordRepeat_" type="text" onpaste="return false;" ondrop="return false;" autocomplete="off" label-placement="floating" label="Repeat password" [type]="true ? 'password' : 'text'" required></ion-input>
-                <div *ngIf="APasswordRepeat.invalid && APasswordRepeat.touched && APassword.value!=APasswordRepeat.value" class="error">
+              <ion-item>
+                <ion-input [formControl]="APasswordRepeat" data-testid="accountPasswordRepeat_" type="text"
+                           onpaste="return false;" ondrop="return false;" autocomplete="off" label-placement="floating"
+                           label="Repeat password" [type]="true ? 'password' : 'text'" required></ion-input>
+                <div
+                  *ngIf="APasswordRepeat.invalid && APasswordRepeat.touched && APassword.value!=APasswordRepeat.value"
+                  class="error">
                   Both passwords must match
                 </div>
               </ion-item>
               <ion-item>
-                <ion-button [disabled]="APasswordRepeat.value!=APassword.value && !(APassword.valid && APasswordRepeat.valid && ADate.valid && AEmail.valid && AName.valid)" (click)="createAccount()" data-testid="accountCreateBTN_"
-                >Create Account</ion-button>
+                <ion-checkbox (ionChange)="readTermsOfSerice()">I have read and agree to the terms of serivce
+                </ion-checkbox>
+              </ion-item>
+              <ion-item>
+                <ion-button
+                  [disabled]="APasswordRepeat.value!=APassword.value && !(APassword.valid && APasswordRepeat.valid && ADate.valid && AEmail.valid && AName.valid && haveRead) "
+                  (click)="createAccount()" data-testid="accountCreateBTN_"
+                >Create Account
+                </ion-button>
               </ion-item>
             </ion-col>
             <ion-col>
-              <ion-grid >
-                <ion-row >
+              <ion-grid>
+                <ion-row>
                   <ion-col class="grid-item">
                     <ion-img class="profile-img" style="width: 30%" [src]="currentAvatarUrl"/>
                   </ion-col>
@@ -76,13 +92,14 @@ import {environment} from "../../environments/environment.prod";
           <a href="termsofservice">read our terms of service here</a>
         </ion-item>
       </ion-content>
-        `,
+    `,
 })
 export class RegisterPage {
 
   hide = true;
   defaultAvatarUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/2048px-Default_pfp.svg.png";
   currentAvatarUrl = this.defaultAvatarUrl;
+  haveRead = false;
 
 
   AName = new FormControl("",[Validators.required,Validators.minLength(1),Validators.maxLength(100)]);
@@ -128,6 +145,18 @@ export class RegisterPage {
 
     }
     this.router.navigateByUrl("login");
+  }
+
+  readTermsOfSerice()
+  {
+    if(this.haveRead == false)
+    {
+      this.haveRead = true;
+    }
+    else
+    {
+      this.haveRead = false;
+    }
   }
 
 }
