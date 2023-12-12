99cccb8fbec51fff4f217a53263f01acacdac666
andreas14101 <andreasshansen@live.dk>
2023-12-12 10:20:41 +0100

    refactor of terms of service

--- frontend/src/app/TermsofSerivcePage/termsOfService.page.ts
+++ frontend/src/app/TermsofSerivcePage/termsOfService.page.ts
@@ -11,10 +11,12 @@ import {Component} from "@angular/core";
         <ion-content align="center">
           <br>
           <br>
-          <ion-text>KeepSocial is a school project so it might not get any updates or support, please don’t share any sensitive information of any kind on the site.</ion-text>
+          <ion-text>KeepSocial is a school project so it might not get any updates or support, please don’t share any sensitive information of any kind on the site if any sensitive information is shared then KeepSocial is not liable.</ion-text>
           <br>
           <br>
-          <ion-text>Any and all user data will not be shared with or sold to any third party and we have the right to shutdown KeepSocial services for any and all users if we see fit </ion-text>
+          <ion-text>Any and all user data will not be shared with or sold to any third party and we have the right to shutdown KeepSocial services for any and all users if we see fit.</ion-text>
+          <br>
+          <ion-text>If KeepSocial finds that any criminal activity is taken place on the platform then we will report it to the proper authorities.</ion-text>
         </ion-content>
     `,
 })
